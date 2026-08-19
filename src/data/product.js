b1250ddbd93a56bb4323597d6a4a92abccddb6f6
deveshsharma125 <deveshsharma0306@gmail.com>
2026-08-19 15:03:41 +0530

    feat: setup frontend project structure

---
+++ src/data/product.js
@@ -0,0 +1,16 @@
+const products = [
+  {
+    id: "P001",
+    name: "Amul Milk",
+    category: "Dairy",
+    stock: 100,
+  },
+  {
+    id: "P002",
+    name: "Maggi",
+    category: "Instant Food",
+    stock: 50,
+  },
+];
+
+export default products;
