778a437dd06a3a56c26cceb491c10f731db5fb49
Sankriti <sanskritikant2@gmail.com>
2026-08-25 16:12:45 +0530

    setup PostgreSQL database connection

---
+++ Server/src/config/db.js
@@ -0,0 +1,22 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+console.log("DB USER:", process.env.DB_USER);
+console.log("DB NAME:", process.env.DB_NAME);
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+
+pool.on("connect", () => {
+    console.log("PostgreSQL connected successfully");
+});
+
+pool.on("error", (error) => {
+    console.error("PostgreSQL connection error:", error.message);
+});
+
+module.exports = pool;
